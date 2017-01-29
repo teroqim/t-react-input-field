545b1d31f293c59391c30ec20b02c41aa9f243fb
teroqim <teroqim@gmail.com>
2017-01-29 01:23:42 +0100

    Add export

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index.js",
+    library: 'InputField',
+    libraryTarget: 'umd'
   },
   module: {
     loaders: [
